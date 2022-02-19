6fd150bdee669cbd7480cfcd61764db84a44861f
kasra paketeghad <kasrapak69@gmail.com>
2022-02-19 12:20:53 +0330

    added active states and fixed hero image responsiveness

--- script.js
+++ script.js
@@ -4,9 +4,11 @@ const mobileMenu = document.querySelector('.mobile-menu')
 hamburgerBtn.addEventListener('click', () => {
   if (hamburgerBtn.ariaSelected) {
     mobileMenu.style.transform = 'translateX(100%)'
+    hamburgerBtn.style.background = "url('/images/icon-hamburger.svg') no-repeat center"
     hamburgerBtn.ariaSelected = false
   } else {
     mobileMenu.style.transform = 'translateX(0)'
+    hamburgerBtn.style.background = "url('/images/icon-close.svg') no-repeat center"
     hamburgerBtn.ariaSelected = true
   }
 })
